991086d50fbe9bddf0d0cbed1fd492925ddde2b8
Binod Shrestha <binodnme08@gmail.com>
2020-02-22 11:14:35 +0545

    update methods to accept fields as parameter

--- libgen.go
+++ libgen.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -14,12 +15,14 @@ const baseURL = "http://libgen.is/"
 
 const searchURL = baseURL + "search.php?lg_topic=libgen&open=0&view=simple&res=100&phrase=1&column=def&"
 
-const jsonURL = baseURL + "json.php?fields=id,author,title,md5,year,extension,filesize&"
+const defaultFields = "id,author,title,md5,year,extension,filesize"
 
-const downloadURL = baseURL + "get.php?md5="
+const jsonURL = baseURL + "json.php"
+
+const downloadURL = baseURL + "get.php"
 
 //SearchBookByTitle returns the list of BookInfo which contains the search string
-func SearchBookByTitle(searchStr string) []BookInfo {
+func SearchBookByTitle(searchStr string, fields string) []BookInfo {
 	// URL encode given search string
 	value := url.Values{"req": {searchStr}}
 	requestURL := searchURL + value.Encode()
@@ -30,7 +33,6 @@ func SearchBookByTitle(searchStr string) []BookInfo {
 	}
 
 	var ids string
-
 	doc.Find(".c > tbody:nth-child(1) > tr").Each(func(i int, s *goquery.Selection) {
 		//reach to id column
 		id := s.Find("td:nth-child(1)").Text()
@@ -43,21 +45,30 @@ func SearchBookByTitle(searchStr string) []BookInfo {
 		return []BookInfo{}
 	}
 
-	books, err := FindBooksByIds(ids)
+	books, err := FindBooksByIds(ids, fields)
 
 	if err != nil {
 		return []BookInfo{}
 	}
 
 	for i, book := range books {
-		books[i].DownloadLink = downloadURL + book.MD5
+		books[i].DownloadLink = downloadURL + "?md5=" + book.MD5
 	}
 
 	return books
 }
 
-func FindBooksByIds(ids string) ([]BookInfo, error) {
-	requestURL := jsonURL + "ids=" + ids
+func FindBooksByIds(ids string, fields string) ([]BookInfo, error) {
+
+	if strings.TrimSpace(fields) == "" {
+		fields = defaultFields
+	}
+
+	params := url.Values{"ids": {ids}, "fields": {fields}}
+
+	requestURL := jsonURL + "?" + params.Encode()
+	fmt.Println("request url by ids " + requestURL)
+
 	res, err := http.Get(requestURL)
 
 	if err != nil {
--- libgen_test.go
+++ libgen_test.go
@@ -6,9 +6,10 @@ import (
 )
 
 func TestAbs(t *testing.T) {
-	books := SearchBookByTitle("attitude is")
+	books := SearchBookByTitle("attitude is", "")
 
 	for _, book := range books {
-		fmt.Printf("Title: %s\nAuthor: %s\nExtension: %s\nDownload Link: %s\n\n", book.Title, book.Author, book.FileExtension, book.DownloadLink)
+		fmt.Println(book)
+		fmt.Println()
 	}
 }
